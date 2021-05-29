299cf54359fde15aab2d4195ce264decac7158d5
Fabien Zucchet <fabien.zucchet@viarezo.fr>
2021-05-29 22:25:07 +0200

    Add router

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Apprendre React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/url/list">
+            <UrlList />
+          </Route>
+          <Route path="/url/edit/:id">
+            <UrlEdit />
+          </Route>
+          <Route path="/url/view/:id">
+            <UrlView />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function UrlList() {
+  return <h2>UrlList</h2>;
+}
+
+function UrlEdit() {
+  const { id } = useParams();
+
+  return <h2>UrlEdit { id }</h2>;
+}
+
+function UrlView() {
+  const { id } = useParams();
+
+  return <h2>UrlView {id}</h2>;
 }
 
 export default App;
