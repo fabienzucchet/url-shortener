a0b40b3d47dc31000219b028ea029314f478d3f4
Clifward <23173171+clifward@users.noreply.github.com>
2021-06-09 17:12:31 +0200

    Use JWT to authenticate the current user instead of a parameter (#8)
    * use jwt to know the identity of the current user instead of a parameter
    
    * improvement on the url ownership

--- api/app/routers/url.py
+++ api/app/routers/url.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 
 from fastapi import FastAPI, Depends, Request, HTTPException, Cookie, Response, status, Header, APIRouter
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_403_FORBIDDEN
 
 from ..datastores.db import crud, schemas
 from ..dependencies import get_db, get_timeseries_client, get_current_user
@@ -21,36 +22,44 @@ router = APIRouter(prefix="/url", tags=["url"])
 
 
 @router.get("/", response_model=List[schemas.Url])
-async def get_my_urls(username: str, db: Session = Depends(get_db)):
-    return crud.get_user_urls(db=db, username=username)
+async def get_my_urls(db: Session = Depends(get_db), user=Depends(get_current_user)):
+    return crud.get_user_urls(db=db, username=user["login"])
 
 
 @router.put("/", response_model=schemas.Url, status_code=status.HTTP_201_CREATED)
-async def put_url(url: schemas.UrlCreate, db: Session = Depends(get_db)):
+async def put_url(url: schemas.UrlBase, db: Session = Depends(get_db), user=Depends(get_current_user)):
     if crud.get_url_by_short_url(db=db, short_url=url.short_url):
         raise HTTPException(
             status_code=400, detail="Short url already registered")
+    url = schemas.UrlCreate(**url.dict(), owner_username=user["login"])
     return crud.put_url(db=db, url=url)
 
 
 @router.get("/id/{url_id}", response_model=EnrichedUrl)
-async def get_url_by_id(url_id: int, db: Session = Depends(get_db), tsdb=Depends(get_timeseries_client), start: int = -60, step: int = 5):
-    resp = crud.get_url_by_id(db=db, url_id=url_id)
+async def get_url_by_id(url_id: int, db: Session = Depends(get_db), tsdb=Depends(get_timeseries_client), start: int = -60, step: int = 5, user=Depends(get_current_user)):
+    url = crud.get_url_by_id(db=db, url_id=url_id)
+    if url.owner_username != user["login"]:
+        raise HTTPException(
+            status_code=403, detail="You are not the owner of this URL")
     stats = []
-    if resp is not None:
+    if url is not None:
         stats = queries.get_stats_url(
             q=tsdb.query_api(),
             _start=datetime.timedelta(minutes=start),
             _every=datetime.timedelta(minutes=step),
             url_id=url_id
         )
-    enriched_url = EnrichedUrl(url=resp, stats=stats)
+    enriched_url = EnrichedUrl(url=url, stats=stats)
     return enriched_url
 
 
 @router.delete("/{url_id}", response_model=schemas.Url)
-async def delete_url(url_id: int, db: Session = Depends(get_db)):
-    if crud.get_url_by_id(db=db, url_id=url_id) is None:
+async def delete_url(url_id: int, db: Session = Depends(get_db), user=Depends(get_current_user)):
+    url = crud.get_url_by_id(db=db, url_id=url_id)
+    if url is None:
         raise HTTPException(
             status_code=400, detail="There is no such URL to delete")
+    if url.owner_username != user["login"]:
+        raise HTTPException(
+            status_code=403, detail="You are not the owner of this URL")
     return crud.delete_url(db=db, url_id=url_id)
